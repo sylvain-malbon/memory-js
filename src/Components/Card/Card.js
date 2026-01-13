af2515f3603ae6cacf1e76999fc6e88a5a250dbb
sylvain-malbon <sylvain.malbon@laplateforme.io>
2026-01-13 15:07:31 +0100

    Refactor des composants Card pour intégrer un flip 3D et améliorer la structure des faces de carte

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 function Card({ image, flipped, matched, onClick }) {
     return (
-        <div className="card" onClick={onClick}>
-            {flipped || matched ? (
-                <span className="emoji" aria-label="carte">{image}</span>
-            ) : (
-                <div className="back"></div>
-            )}
+        <div className={`card${flipped || matched ? ' flipped' : ''}`} onClick={onClick}>
+            <div className="card-inner">
+
+                {/* Face avant (emoji) */}
+                <div className="card-front">
+                    <span className="emoji" aria-label="carte">{image}</span>
+                </div>
+
+                {/* Face arrière (dos de la carte) */}
+                <div className="card-back"></div>
+
+            </div>
         </div>
     );
 }
